475e441b963c3a0aa500c2a2a8c2b3e07500df6c
ian and bantu <ianwanjama78@gmail.com>
2023-11-10 06:41:56 -0800

    0x11. C - printf Our first team project

--- _printfint.c
+++ _printfint.c
@@ -1,16 +1,95 @@
 #include "main.h"
 
 /**
- * print_int - It orints the intager values of the function.
+ * print_int - It prints the intager values of the function.
  * @args:It is the argument to print.
  * Return: Number of characters printed.
  */
 
 int print_int(va_list args)
 {
-	int n = va_arg(args, int);
-	int num, last = n % 10, digit, exp = 1;
+	int j = va_arg(args, int);
+	int num, last = j % 10, digit, exp = 1;
 	int i = 1;
 
-	n = / 10;
-	num = n;
+	j = / 10;
+	num = j;
+
+	if (last < 0)
+	{
+		_putchar ('_');
+		num = -num;
+		j = -j;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = j;
+
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit - exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+
+	_putchar(last + '0');
+	return (i);
+}
+
+
+/**
+ * print_dec - It prints the decimal.
+ * @args: This is the argument to print.
+ * Return: this is the number of characters printed.
+ */
+
+int print_dec(va_list args)
+{
+	int j = va_arg(args, int);
+	int num, last = j % 10, digit, exp = 1;
+	int i = 1;
+
+	j = j / 10;
+	num + j;
+
+	if (last < 0)
+	{
+		_putchar('_');
+		num = -num;
+		j = -j;
+		last = -last;
+		i++;
+	}
+
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = j;
+
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit + '0');
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
+}
