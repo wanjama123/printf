a10c2a62bb379dcf123354d6183c5dbf57c7b300
Bantu-art <brianbaraza40@gmail.com>
2023-11-10 06:56:47 -0800

    0x11. C - printf our first team project

--- main.h
+++ main.h
@@ -8,5 +8,7 @@ int printf_string(va_list val);
 int _strlen(char *str);
 int _strlenc(const char *str);
 int print_percent(void);
+int print_int(va_list args);
+int print_dec(va_list args);
 
 #endif
