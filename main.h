86ef5a9acc9e0d262c3b444df9f99b5cb6026694
ian and bantu <ianwanjama78@gmail.com>
2023-11-12 06:10:07 -0800

    0x11. C - printf Our first team project

--- main.h
+++ main.h
@@ -13,5 +13,8 @@ int print_dec(va_list args);
 int print_bin(va_list val);
 int print_unsigned(va_list args);
 int print_oct(va_list val);
+int print_hex(va_list val);
+int print_HEX(va_list val);
+int print_exc_string(va_list val);
 
 #endif
