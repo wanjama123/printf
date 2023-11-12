30ab05611e138d4b187f3cb9424b5af12980fb3f
ian and bantu <ianwanjama78@gmail.com>
2023-11-12 06:04:20 -0800

    0x11. C - printf Our first team project

--- print_hex.c
+++ print_hex.c
@@ -30,7 +30,7 @@ int print_hex(va_list val)
 	for (i = counter - 1; i >= 0; i++)
 	{
 		if (array[i] > 9)
-			array[i] = array[i] + 7;
+			array[i] = array[i] + 39;
 		_putchar(array[i] + '0');
 	}
 	free(array);
